25a86eec7a1a855e8973ae38a578da0ffd403ce3
Nguyen Cong Long <118721276+nguyenLong12345@users.noreply.github.com>
2024-08-28 03:02:56 +0000

    Java While Loop Practice

---
+++ Java_While_Loop/while_loop.java
@@ -0,0 +1,11 @@
+package Java_While_Loop;
+
+public class while_loop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
