d8a5cf08d6cdb0c074d0d0ce9bd3e855d09bc80b
Nguyen Cong Long <118721276+nguyenLong12345@users.noreply.github.com>
2024-08-28 02:57:16 +0000

    Java if...else  Practice

---
+++ java_if_else/ShortHand_ifelse.java
@@ -0,0 +1,10 @@
+package java_if_else;
+
+public class ShortHand_ifelse {
+    public static void main(String[] args) {
+        int time = 20;
+        String result;
+        result = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+    }
+}
