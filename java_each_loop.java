3477ce5b31211c53ab60a358101a7ec8fb04f07d
chitai1844 <23010548@st.phenikaa-uni.edu.vn>
2024-08-29 12:43:58 +0700

    Add files via upload

---
+++ java_each_loop.java
@@ -0,0 +1,8 @@
+public class java_each_loop {
+    public static void main(String[] args) {
+        String[] c = { "Sony", "Canon", "Niko", "Fuji" };
+        for (String i : c) {
+            System.out.println(i);
+        }
+    }
+}
