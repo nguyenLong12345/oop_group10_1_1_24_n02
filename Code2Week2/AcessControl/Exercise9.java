c051c1dc51bdfe44efc8829b66f800ee64aef33e
Nguyen Cong Long <118721276+nguyenLong12345@users.noreply.github.com>
2024-09-01 10:17:07 +0000

    xong c1 Code 1 Week 2

---
+++ Code2Week2/AcessControl/Exercise9.java
@@ -0,0 +1,7 @@
+package Code2Week2.AcessControl;
+
+public class Exercise9 {
+    /* - tại vì nó không cùng chung 1 package nên việc tạo 1 đối tượng ngoài package hiện tại 
+    sẽ xảy ra lỗi không có kiểu dữ liệu đó.
+       - Có. chương trình sẽ không xảy ra lỗi. */
+}
---
+++ Code2Week2/AcessControl/access/foreign/Foreign.java
@@ -0,0 +1,9 @@
+package Code2Week2.AcessControl.access.foreign;
+
+import Code2Week2.AcessControl.access.local.*;
+
+public class Foreign {
+    public static void main(String[] args) {
+        PackagedClass pc = new PackagedClass();
+    }
+}
