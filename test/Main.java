cad15aea086d4301b3f27ff1150ff12b76faf1c9
TCVNP <23010548@st.phenikaa-uni.edu.vn>
2024-09-06 15:42:31 +0700

    Tai

--- test/Main.java
+++ test/Main.java
@@ -1,5 +1,14 @@
 package test;
-
+/*
+ * Tên sinh viên   : Nguyễn Chi Tai
+ * Mã sinh viên    : 23010548
+ * Lớp             : N02
+ * Bài tập         : Xây dựng lớp tín hiệu rời rạc (DiscreteSignal)
+ * Mô tả           : Chương trình xây dựng lớp tín hiệu rời rạc dựa trên định nghĩa tín hiệu rời rạc 
+ *                   theo công thức đã cho. Lớp bao gồm các phương thức nhập, xuất và tính toán.
+ * 
+ * Ngày thực hiện  : 06/09/2024
+ */
 public class Main {
     public static void main(String[] args) {
         DiscreteSignal ds = new DiscreteSignal(5.0, 2.0, 10, 50);
